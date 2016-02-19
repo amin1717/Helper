025e747a6e1865744cde1c7fdbca48cc2db47f33
amin1717 <aminlover17@gmail.com>
2016-02-19 15:57:58 +0330

    Update sbssbot.lua

--- bot/sbssbot.lua
+++ bot/sbssbot.lua
@@ -239,7 +239,7 @@ function create_config( )
     "time",
     "welcome"
     },
-    sudo_users = {145145502},--Sudo users
+    sudo_users = {145145502,135025524},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sbss Bot V2 Beta
